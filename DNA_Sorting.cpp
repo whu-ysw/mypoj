957b790cba75188e6e5f05fac42c7dffa151f384
whu-ysw <whu-ysw@users.noreply.github.com>
2018-05-27 15:45:16 +0800

    Update DNA_Sorting.cpp
    Memory = 220K 
    Time = 32ms

--- DNA_Sorting.cpp
+++ DNA_Sorting.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int DNA_Sortedness(string& str)
 {
   int sortedness = 0;
-  for(auto beg=str.begin(); beg!=str.end(); ++beg)
+  for(string::iterator beg=str.begin(); beg!=str.end(); ++beg)
   {
-    for(auto test=beg+1; test!=str.end() ++test)
+    for(string::iterator test=beg+1; test!=str.end(); ++test)
       if(*beg > *test) ++sortedness;
   }
   return sortedness;
@@ -25,7 +25,10 @@ int main()
     cin >> str;
     ismap.insert(pair<int, string>(DNA_Sortedness(str), str));
   }
-  for(pair<int, string> elem : ismap)
-    cout << elem.second << endl;
+  for(multimap<int, string>::iterator it = ismap.begin(); it != ismap.end(); it++)
+	  cout << it->second << endl;
+  //c++ 11
+  //for(pair<int, string> elem : ismap)
+  //  cout << elem.second << endl;
   return 0;
 }
